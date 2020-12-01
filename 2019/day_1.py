6820e3318081113dcdc7dd8fdc760cfd9105eed4
Domen Mohorcic <d.mohorcic@gmail.com>
2020-12-01 12:08:57 +0100

    Solution for 2019/Day 1

---
+++ 2019/day_1.py
@@ -0,0 +1,33 @@
+import numpy as np
+import math
+
+def task1(arr):
+	print("Task 1")
+	print(sum([math.floor(x/3)-2 for x in arr]))
+
+
+def task2(arr):
+	print("Task 2")
+	s = 0
+	for x in arr:
+		fuel = math.floor(x/3)-2
+		additional_fuel = math.floor(fuel/3)-2
+		while additional_fuel > 0:
+			fuel += additional_fuel
+			additional_fuel = math.floor(additional_fuel/3)-2
+		s += fuel
+	print(s)
+
+
+def main():
+	lst = list()
+	with open("day_1.txt", "r") as f:
+		for l in f:
+			lst.append(int(l.split('\n')[0]))
+	arr = np.array(lst)
+	
+	task1(arr)
+	task2(arr)
+
+if __name__ == "__main__":
+	main()
--- 2020/day_1.py
+++ 2020/day_1.py
@@ -1,23 +1,25 @@
 import numpy as np
 
 def task1(arr):
+	print("Task 1")
 	for i in range(len(arr)):
 		for j in range(i+1, len(arr)):
 			if arr[i]+arr[j] == 2020:
-				print(arr[i], arr[j])
 				print(arr[i]*arr[j])
+				return
 
 def task2(arr):
+	print("Task 2")
 	for i in range(len(arr)):
 		for j in range(i+1, len(arr)):
 			for k in range(j+1, len(arr)):
 				if arr[i]+arr[j]+arr[k] == 2020:
-					print(arr[i], arr[j], arr[k])
 					print(arr[i]*arr[j]*arr[k])
+					return
 
 def main():
 	lst = list()
-	with open("1_input.txt", "r") as f:
+	with open("day_1.txt", "r") as f:
 		for l in f:
 			lst.append(int(l.split('\n')[0]))
 	arr = np.array(lst)
