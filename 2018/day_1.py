ee9ba55aab5d09835a5ac3354df5ebf3ddea83b2
Domen Mohorcic <d.mohorcic@gmail.com>
2020-12-01 12:17:00 +0100

    Solution for 2018/Day 1

---
+++ 2018/day_1.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def task1(arr):
+	print("Taks 1")
+	print(sum(arr))
+
+def task2(arr):
+	print("Task 2")
+	d = dict()
+	f, i = 0, 0
+	d[f] = 1
+	while True:
+		f += arr[i]
+		i += 1
+		if i == len(arr):
+			i = 0
+		if f in d.keys():
+			print(f)
+			return
+		else:
+			d[f] = 1
+
+def main():
+	lst = list()
+	with open("day_1.txt", "r") as f:
+		for l in f:
+			lst.append(int(l.split('\n')[0]))
+	arr = np.array(lst)
+
+	task1(arr)
+	task2(arr)
+
+if __name__ == "__main__":
+	main()
