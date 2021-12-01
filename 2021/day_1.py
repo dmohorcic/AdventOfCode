a6f72a93850e7cc843a35223f98a208c0e962751
Domen Mohorcic <d.mohorcic@gmail.com>
2021-12-02 00:17:34 +0100

    2021 day 1

---
+++ 2021/day_1.py
@@ -0,0 +1,24 @@
+def task1(arr):
+	increases = 0
+	for i in range(1, len(arr)):
+		if arr[i] > arr[i-1]:
+			increases += 1
+	return increases
+
+def task2(arr):
+	increases = 0
+	for i in range(4, len(arr)+1):
+		prev = sum(arr[i-4:i-1])
+		next = sum(arr[i-3:i])
+		if next > prev:
+			increases += 1
+	return increases
+
+if __name__ == "__main__":
+	arr = list()
+	with open("2021/day_1.in", "r") as f:
+		for l in f.readlines():
+			arr.append(int(l))
+
+	print(task1(arr))
+	print(task2(arr))
